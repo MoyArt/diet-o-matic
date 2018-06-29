0597f6199093e84f8588a51176dab69e4419250f
MoyArt <moyart@gmail.com>
2018-06-29 11:19:14 -0500

    Added data conection and suscription from dialog buttons to current training component

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -22,10 +22,13 @@ export class CurrentTrainingComponent implements OnInit {
   };
   onStop(){
     clearInterval(this.timer);
-    this.dialog.open(StopTrainingComponent, {
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
       data:{
         progess: this.progress
       }
+    });
+    dialogRef.afterClosed().subscribe(result=>{
+      console.log(result)
     });
   }
 }
