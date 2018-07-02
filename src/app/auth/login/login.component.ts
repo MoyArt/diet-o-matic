f2cb0f0628852399d0dac69d9ac301522f57b48b
MoyArt <moyart@gmail.com>
2018-07-02 16:49:00 -0500

    Added Auth service to login and signup

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms'
+import { AuthService } from '../auth.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +9,23 @@ import { NgForm } from '@angular/forms'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', { 
+        validators: [Validators.required, Validators.email]
+      }),
+      password: new FormControl('', {
+        validators: [Validators.required]
+      })
+    })
   }
   onSubmit(form:NgForm){
-    console.log(form)
+    this.authService.loginUser({
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    });
   }
 }
