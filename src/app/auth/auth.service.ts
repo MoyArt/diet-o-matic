525a57ebf5c47befdb1bddf52d66f9d943d83e00
MoyArt <moyart@gmail.com>
2018-07-09 08:33:21 -0500

    Adding router to Auth component

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,33 +1,45 @@
 import { Subject } from 'rxjs/Subject';
+import { Router } from '@angular/router';
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
+import { Injectable } from '@angular/core';
+
+@Injectable()
 
 export class AuthService{
     authChange = new Subject<boolean>();
     private user: User;
+    constructor(private router:Router){
+
+    }
 
     registerUser(authData: AuthData){
         this.user = {
             email: authData.email,
             userId: Math.round(Math.random() * 1000).toString()
         };
-        this.authChange.next(true);
+        this.loginSuccess()
     };
     loginUser(authData: AuthData){
         this.user = {
             email: authData.email,
             userId: Math.round(Math.random() * 1000).toString()
         };
-        this.authChange.next(true);
+        this.loginSuccess()
     };
     logoutUser(authData: AuthData){
         this.user = null;
         this.authChange.next(false);
+        this.router.navigate(['/login']);
     };
     getUser(){
         return {...this.user};
     };
     isAuth(){
         return this.user != null;
+    }
+    loginSuccess(){
+        this.authChange.next(true);
+        this.router.navigate(['/training']);
     }
 }
