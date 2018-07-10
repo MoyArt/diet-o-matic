1cece2813a55411decc714fef7443e21c231ce21
MoyArt <moyart@gmail.com>
2018-07-10 08:44:23 -0500

    Added router to auth.guard.ts

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 export class AuthGuard implements CanActivate{
-    constructor(private authService: AuthService){
-        
+    constructor(private authService: AuthService){     
     }
     canActivate(route:ActivatedRouteSnapshot, state:RouterStateSnapshot){
-        return true;
+        if(this.authService.isAuth()){
+            return true;
+        } else {
+            this.router.navigate(['/login']);
+        }
     }
 }
