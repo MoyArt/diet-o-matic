16759aa1aa0e96fcf4eb47e7fb311d08f337edd9
MoyArt <moyart@gmail.com>
2018-07-10 08:37:57 -0500

    Added auth service to auth.guard.ts

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,7 +1,11 @@
+import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-
+import { AuthService } from './auth.service';
 
 export class AuthGuard implements CanActivate{
+    constructor(private authService: AuthService){
+        
+    }
     canActivate(route:ActivatedRouteSnapshot, state:RouterStateSnapshot){
         return true;
     }
