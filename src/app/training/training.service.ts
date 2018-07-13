c722b2280f208b08d1cb8294bfa438e742433a87
MoyArt <moyart@gmail.com>
2018-07-13 08:28:00 -0500

    Added Training Service

---
+++ src/app/training/training.service.ts
@@ -0,0 +1,10 @@
+import { Excersise } from "./excercise.model";
+
+export class TrainingService {
+    availableExercises: Excersise[] = [
+        { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+        { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
+        { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
+        { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
+    ]
+}
