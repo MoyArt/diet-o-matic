66339d055339f13cbfbfb57fe9e41bdb3a44510d
MoyArt <moyart@gmail.com>
2018-07-18 09:56:27 -0500

    Added runningExercise and startExercise to training.service

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -8,7 +8,12 @@ export class TrainingService {
         { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 },
         { id: 'planks', name: 'Planks', duration: 90, calories: 30 }
     ];
+    private runningExercise: Exercise;
+
     getAvailableExercises(){
         return this.availableExercises.slice();
+    }
+    startExercise(selectedId: string){
+        this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId)
     }
 }
