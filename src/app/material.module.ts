3f7c4cb6c4ea88cf470672f0ebbfa0603f415801
MoyArt <moyart@gmail.com>
2018-08-06 09:36:37 -0500

    Added material table to past trainings, to show past information

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -13,7 +13,8 @@ import { MatButtonModule,
         MatCardModule,
         MatSelectModule,
         MatProgressSpinnerModule,
-        MatDialogModule } from '@angular/material';
+        MatDialogModule, 
+        MatTableModule } from '@angular/material';
 
 
 @NgModule({
@@ -32,7 +33,8 @@ import { MatButtonModule,
         MatCardModule,
         MatSelectModule,
         MatProgressSpinnerModule,
-        MatDialogModule
+        MatDialogModule, 
+        MatTableModule
     ],
     exports: [
         MatButtonModule,
@@ -49,7 +51,8 @@ import { MatButtonModule,
         MatCardModule,
         MatSelectModule,
         MatProgressSpinnerModule,
-        MatDialogModule
+        MatDialogModule, 
+        MatTableModule
     ]
 })
 
