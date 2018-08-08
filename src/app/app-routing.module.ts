36a82cd92298f79502baf9b8c40c475269d53c2c
MoyArt <moyart@gmail.com>
2018-08-08 18:02:34 -0500

    Added Auth Routing Module to manage routes for Auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WellcomeComponent } from './wellcome/wellcome.component'
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training.component';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
     {path: '', component: WellcomeComponent},
-    {path: 'signup', component: SignupComponent},
-    {path: 'login', component: LoginComponent},
     {path: 'training', component: TrainingComponent, canActivate: [AuthGuard]}
 ];
 
