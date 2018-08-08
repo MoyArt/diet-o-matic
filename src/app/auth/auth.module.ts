f28c30699027614fe46e0fe62fe92cd6caba4904
MoyArt <moyart@gmail.com>
2018-08-08 17:52:19 -0500

    Added shared Module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '../material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireAuthModule} from "angularfire2/auth";
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -13,14 +12,10 @@ import { AngularFireAuthModule} from "angularfire2/auth";
         LoginComponent 
     ],
     imports: [ 
-        CommonModule,
-        FormsModule,
         ReactiveFormsModule,
-        MaterialModule,
-        FlexLayoutModule,
-        AngularFireAuthModule
-    ],
-    exports: [ ]
+        AngularFireAuthModule,
+        SharedModule
+    ]
 })
 
 export class AuthModule {}
