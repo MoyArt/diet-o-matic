65a4e90f51f3fc4ad52ad74567d7842152187bfe
MoyArt <moyart@gmail.com>
2018-08-10 09:10:00 -0500

    Production environment setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC_456gkAPyCwp1cTUmDiFa0-sTDJJg9Nc",
+    authDomain: "diet-o-matic.firebaseapp.com",
+    databaseURL: "https://diet-o-matic.firebaseio.com",
+    projectId: "diet-o-matic",
+    storageBucket: "diet-o-matic.appspot.com",
+    messagingSenderId: "388832889086"
+  }
 };
