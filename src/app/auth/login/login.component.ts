dc64fb68b18b8cd208a2e937c2909a733ae0b225
MoyArt <moyart@gmail.com>
2018-08-13 13:06:52 -0500

    turn isLoading to an observable and added to component html

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,28 +1,30 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms'
 import { AuthService } from '../auth.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { UIService } from '../../shared/ui.service';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../../app.reducer';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit, OnDestroy {
+export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  isLoading = false;
+  isLoading$: Observable<boolean>;
   private loadingSub: Subscription;
   constructor(private authService: AuthService, private uiService: UIService, private store: Store<{ui: fromApp.State}>) { }
 
   ngOnInit() {
-    this.store.subscribe(data => console.log(data))
-    this.loadingSub = this.uiService.loadingStateChanged.subscribe(isLoading => {
+    this.isLoading$ = this.store.map( state => state.ui.isLoading);
+    /* this.loadingSub = this.uiService.loadingStateChanged.subscribe(isLoading => {
       this.isLoading = isLoading;
-    })
+    }) */
     this.loginForm = new FormGroup({
       email: new FormControl('', { 
         validators: [Validators.required, Validators.email]
@@ -38,10 +40,10 @@ export class LoginComponent implements OnInit, OnDestroy {
       password: form.value.loginPassword
     });
   }
-  ngOnDestroy(){
+/*   ngOnDestroy(){
     if(this.loadingSub){
       this.loadingSub.unsubscribe()
     }
     
-  }
+  } */
 }
